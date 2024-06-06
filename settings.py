6b9680a0faf7881a9644d5d814f30bf81570a564
KamilRahmatullin <kamil.raxmatullin.01@bk.ru>
2024-06-06 22:49:47 +0300

    Poetry add

--- settings.py
+++ settings.py
@@ -6,5 +6,10 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://postgres:kamil4015@127.0.0.1:5432/postgres"
+    default="postgresql+asyncpg://postgres:postgres@localhost:5434/postgres"
+)  # connect string for the database
+
+TEST_DATABASE_URL = env.str(
+    "TEST_DATABASE_URL",
+    default="postgresql+asyncpg://postgres_test:postgres_test@localhost:5433/postgres_test"
 )  # connect string for the database
