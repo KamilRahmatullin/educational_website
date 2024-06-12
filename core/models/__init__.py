31011f41645928105cb55dae48d8d92ac8deec8b
KamilRahmatullin <kamil.raxmatullin.01@bk.ru>
2024-06-13 00:38:53 +0300

    Create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,7 @@
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
+from .user import User
 
 
-__all__ = ("Base", "Product", "db_helper", "DatabaseHelper")
+__all__ = ("Base", "Product", "db_helper", "DatabaseHelper", "User")
