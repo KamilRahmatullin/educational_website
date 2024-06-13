2a2e04cc687d6c47b82b2ec7eafcd4085f0e5066
KamilRahmatullin <kamil.raxmatullin.01@bk.ru>
2024-06-13 10:26:15 +0300

    Create foreign key

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,6 +2,7 @@ from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
 from .user import User
+from .post import Post
 
 
-__all__ = ("Base", "Product", "db_helper", "DatabaseHelper", "User")
+__all__ = ("Base", "Product", "db_helper", "DatabaseHelper", "User", "Post")
