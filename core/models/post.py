8a9e57bbb93975b31024d09c0e05772c485bfcec
KamilRahmatullin <kamil.raxmatullin.01@bk.ru>
2024-06-13 10:50:31 +0300

    Create relationships between user and posts

--- core/models/post.py
+++ core/models/post.py
@@ -13,5 +13,5 @@ class Post(Base):
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
 
-    # used_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    # user: Mapped["User"] = relationship(back_populates="posts")
+    used_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
