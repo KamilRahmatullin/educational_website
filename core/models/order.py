979fb16e54b50e6ea3b8b553f808f8856e15be54
KamilRahmatullin <kamil.raxmatullin.01@bk.ru>
2024-06-15 23:10:05 +0300

    New many to many relations

--- core/models/order.py
+++ core/models/order.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+
+if TYPE_CHECKING:
+    from .product import Product
+    from .order_product_association import OrderProductAssociation
 
 
 class Order(Base):
@@ -12,3 +18,16 @@ class Order(Base):
         server_default=func.now(),
         default=datetime.now()
     )
+    # products: Mapped[list["Product"]] = relationship(
+    #     "Product",
+    #     secondary="order_product_association",
+    #     back_populates="orders"
+    # )
+
+    products_details: Mapped[list["OrderProductAssociation"]] = relationship(back_populates="order")
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.id} {self.created_at}"
+
+    def __repr__(self):
+        return str(self)
--- core/models/product.py
+++ core/models/product.py
@@ -1,13 +1,29 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .order import Order
+    from .order_product_association import OrderProductAssociation
+
 
 class Product(Base):
-    __table_name__ = "products"
-
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
 
+    # orders: Mapped[list["Order"]] = relationship(
+    #     "Order",
+    #     secondary="order_product_association",
+    #     back_populates="products"
+    # )
 
+    orders_details: Mapped[list["OrderProductAssociation"]] = relationship(back_populates="product")
+
+    def __str__(self):
+        return f'{self.__class__.__name__} id={self.id}, name={self.name}, price={self.price}'
+
+    def __repr__(self):
+        return str(self)
